672e17a3751c51dffc7b7e611b2091e97d2799e2
divya prakash <d.prakash1506@gmail.com>
2022-03-18 14:48:04 +0530

    Added header and balance components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Header from "./Component/Header";
+import Balance from "./Component/Balance";
 
 function App() {
   return (
-    <div >
-      Hello World
+    <div>
+      <Header />
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
